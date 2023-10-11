1da8f694a2d99b58ccb67edbe42607179bfe41ee
ThiliniPD <godaget8@gmail.com>
2023-10-11 18:40:28 +1300

    Exercise4

---
+++ Module1/backend/exercise4.js
@@ -0,0 +1,48 @@
+// This function adds two numbers and 
+// prints the summation to the console.
+function sum(a, b){
+    console.log(a + b)
+    }
+sum(5, 1.2)
+sum(8,-2)
+sum(3, 2000)
+
+// This function substract a number from 
+// another and prints the balance to the 
+// console.
+function balance(x, y){
+    console.log(x - y)
+}
+balance(5, 2)
+balance(350, 233)
+balance(-2, 2)
+
+
+// This function divide a number from 
+// another and prints the result to the 
+// console.
+function division(c, d){
+    console.log(c / d)
+}
+division(5, 2)
+division(12, 5)
+division(339, 3)
+
+// This function multiply two numbers and prints 
+// the result to the console.
+function multiply(e, f){
+    console.log(e * f)
+}
+multiply(2, 4)
+multiply(8, 3)
+multiply(22, 4.6)
+
+
+// This function takes a name as an string 
+// argument and prints “Hello <name>”
+function nameOfThePerson(name){
+    x = name
+    console.log("Hello " + x)
+}
+nameOfThePerson("Thilini")
+
