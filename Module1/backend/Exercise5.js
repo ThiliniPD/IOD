70a07ebdc0b0d6eb28d239c199fe3a777f8ac7e0
ThiliniPD <godaget8@gmail.com>
2023-10-12 17:09:35 +1300

    Finished Exercise 5

---
+++ Module1/backend/Exercise5.js
@@ -0,0 +1,20 @@
+// creating an array of 5 elements
+let fruits = [ "apples", "grapes", "mangoes", "bananas", "strawberries" ];
+console.log(fruits);
+
+
+// replacing the value of the element at position 1 and 4
+fruits[1] = "pears";
+fruits[4] = "watermelons"
+console.log(fruits);
+
+// add a new element to the beginning of array
+fruits.unshift('papayas');
+console.log(fruits);
+
+// remove the element at the end of the array
+fruits.pop();
+console.log(fruits);
+
+// print the final array to the console
+console.log(fruits);
